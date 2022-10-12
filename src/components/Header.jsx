2c13c172c88dc3a18dd05cf610d30eaa4445444d
Kunal Hole <kunalhole538@gmail.com>
2022-10-12 15:46:18 +0530

    Develop Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -54,6 +54,21 @@ const Header = () => {
       </div>
 
       {/* Bottom nav  */}
+      <div className="flex text-sm space-x-3 p-2 pl-6 items-center bg-amazon_blue-light text-white ">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electonics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
 
       <div></div>
     </header>
